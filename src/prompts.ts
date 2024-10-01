7d2cb72b6506357dfe34665b3bc22cba52aa6325
kurlee710 <josephatamaniuk@gmail.com>
2024-10-01 18:34:30 -0400

    created main menu prompts and export mainMenu

--- src/prompts.ts
+++ src/prompts.ts
@@ -113,3 +113,51 @@ async function updateEmployeeRole() {
     employee_id,
   ]);
 }
+
+async function mainMenu() {
+  const { action } = await inquirer.prompt({
+    type: "list",
+    name: "action",
+    message: "What would you like to do?",
+    choices: [
+      "View all departments",
+      "View all roles",
+      "View all employees",
+      "Add a department",
+      "Add a role",
+      "Add an employee",
+      "Update an employee role",
+      "Exit",
+    ],
+  });
+
+  switch (action) {
+    case "View all departments":
+      await viewDepartments();
+      break;
+    case "View all roles":
+      await viewRoles();
+      break;
+    case "View all employees":
+      await viewEmployees();
+      break;
+    case "Add a department":
+      await addDepartment();
+      break;
+    case "Add a role":
+      await addRole();
+      break;
+    case "Add an employee":
+      await addEmployee();
+      break;
+    case "Update an employee role":
+      await updateEmployeeRole();
+      break;
+    case "Exit":
+      console.log("Exiting...");
+      process.exit();
+  }
+  await mainMenu();
+}
+
+export { mainMenu };
