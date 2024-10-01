d7aae39889ec3483e9e8f428ee3dc0d31f7bd5ac
kurlee710 <josephatamaniuk@gmail.com>
2024-10-01 18:29:15 -0400

    added function to veiw roles

--- src/prompts.ts
+++ src/prompts.ts
@@ -7,3 +7,10 @@ async function viewDepartments() {
   const result = await db.query("SELECT * FROM departments");
   console.table(result.rows);
 }
+
+async function viewRoles() {
+  const result = await db.query(
+    "SELECT r.id, r.title, r.salary, d.name AS department FROM roles r JOIN departments d ON r.department_id = d.id"
+  );
+  console.table(result.rows);
+}
