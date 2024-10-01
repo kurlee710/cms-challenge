07f97bd45ef04ea652793d14300cddbfcf60d83b
kurlee710 <josephatamaniuk@gmail.com>
2024-10-01 18:50:41 -0400

    created pool and linked the database

--- src/db.ts
+++ src/db.ts
@@ -3,3 +3,11 @@ import { Pool } from "pg";
 import dotenv from "dotenv";
 
 dotenv.config(); // Load environment variables from .env file
+
+const pool = new Pool({
+  user: process.env.DB_USER || "your_default_db_user", // Use the DB_USER environment variable
+  host: "localhost",
+  database: process.env.DB_NAME || "your_default_db_name", // Use the DB_NAME environment variable
+  password: process.env.DB_PASSWORD || "", // Use the DB_PASSWORD environment variable
+  port: 5432,
+});
