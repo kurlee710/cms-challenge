ca83b9e3b6fcf1d14678e4a62fbea7a573ce501a
kurlee710 <josephatamaniuk@gmail.com>
2024-10-01 18:28:39 -0400

    created function to veiw departments

--- src/prompts.ts
+++ src/prompts.ts
@@ -2,3 +2,8 @@
 import inquirer from "inquirer";
 import db from "./db";
 import { Employee } from "./models";
+
+async function viewDepartments() {
+  const result = await db.query("SELECT * FROM departments");
+  console.table(result.rows);
+}
