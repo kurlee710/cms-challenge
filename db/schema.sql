5c80d63079a59b576f074be79b150e9e55492ece
kurlee710 <josephatamaniuk@gmail.com>
2024-10-01 18:21:15 -0400

    created table roles

--- db/schema.sql
+++ db/schema.sql
@@ -7,3 +7,9 @@ CREATE TABLE departments (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL
 );
+CREATE TABLE roles (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER REFERENCES departments(id)
+);
